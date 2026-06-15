015803d5f04bb79710de6162d30ac6ae871f69ec
Gabriel Giglio <giglio.gabriel1@yahoo.com.br>
2026-06-15 16:43:11 -0300

    Atualização UX

--- src/domain/factories.py
+++ src/domain/factories.py
@@ -1,11 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from .entities import Session
 from .strategies import DurationStrategy
 
 class SessionFactory:
     @staticmethod
     def create(therapist_id: str, patient_id: str, start: datetime, strategy: DurationStrategy) -> Session:
-        if start < datetime.now():
+        brazil_now = datetime.utcnow() - timedelta(hours=3)
+        
+        if start < brazil_now:
             raise ValueError("Past date not allowed")
             
         end = start + strategy.get_duration()
